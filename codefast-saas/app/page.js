fa20997ed8b0b47ad1d23672a1cb018659b93cd3
Lohit <lohitofficial1@gmail.com>
2025-02-09 04:50:21 +0530

    New

---
+++ codefast-saas/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <button class="btn btn-secondary">Secondary</button>
+      <button class="btn btn-accent">Accent</button>
+      <button class="btn btn-ghost">Ghost</button>
+      <button class="btn btn-link">Link</button>
+    </main>
+  );
+}
