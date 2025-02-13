83bb25b67e0ee88c007321a1f64b7d80822793dd
Lohit <lohitofficial1@gmail.com>
2025-02-13 10:18:03 +0530

    Updated Hero Section

--- codefast-saas/app/page.js
+++ codefast-saas/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1>Collect Customer feedback to build a better product</h1>
       <button class="btn btn-secondary">Secondary</button>
       <button class="btn btn-accent">Accent</button>
       <button class="btn btn-ghost">Ghost</button>
