f1e4f959854e3bcbe6e9ede68283d37ba4829c4f
Lohit <lohitofficial1@gmail.com>
2025-02-22 10:57:15 +0530

    Update Hero Section

--- codefast-saas/app/dashboard/page.js
+++ codefast-saas/app/dashboard/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function dashboard() {
   return (
     <main>
-      <h1>Welcome to my Dashboard</h1>
+      <h1>collect customer feedback to build better product</h1>
+      <div>
+        Create a feedback board in a minute, priorties features and build
+        product that your customers will love
+      </div>
     </main>
   );
 }
